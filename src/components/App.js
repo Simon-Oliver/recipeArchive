ca5874a6f6779ccf9d062ecbd30bc775aed69c77
Simon-Oliver <stauffer.simon@outlook.com>
2019-03-28 16:11:53 +1100

    only show error component if there is an error

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import List from './List';
 import Form from './Form';
 import Posts from './Posts';
 import Search from './Search';
 import Error from './Error';
 
-const App = () => (
+const mapStateToProps = state => ({ isError: state.isError });
+
+const App = props => (
   <div className="ui container">
     <Search />
     <List />
     <Form />
-    <Error />
+    {props.isError && <Error />}
     <Posts />
   </div>
 );
 
-export default App;
+export default connect(mapStateToProps)(App);
