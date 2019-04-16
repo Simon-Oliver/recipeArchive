d41b46b9dc8c83f8dd5cacdae942f6a1b2ec2159
Simon-Oliver <stauffer.simon@outlook.com>
2019-04-16 21:05:33 +1000

    played around with test

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,12 @@
 const add = (a, b) => a + b;
+const genGreeting = name => `Hello ${name}!`;
 
 test('should add two numbers', () => {
   const result = add(2, 3);
   expect(result).toBe(5);
+});
+
+test('should return string containing name', () => {
+  const result = genGreeting('Lukas');
+  expect(result).toBe('Hello Lukas!');
 });
