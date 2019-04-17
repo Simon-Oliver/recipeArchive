e7b94e8ce0402316122b076ed2f153ab0fdfe41c
Simon-Oliver <stauffer.simon@outlook.com>
2019-04-17 20:23:47 +1000

    added tests for create, edit and update actions

---
+++ src/tests/actions/index.test.js
@@ -0,0 +1,23 @@
+import { addRecipe, editRecipe, deleteRecipe } from '../../actions';
+
+test('should setup remove recipe action object', () => {
+  const action = deleteRecipe('123abc');
+  expect(action).toEqual({ type: 'DELETE_RECIPE', id: '123abc' });
+});
+
+test('should setup edit recipe action object', () => {
+  const action = editRecipe('123abc', { title: 'New Recipe', yield: '10pax' });
+  expect(action).toEqual({
+    type: 'EDIT_RECIPE',
+    id: '123abc',
+    update: { title: 'New Recipe', yield: '10pax' }
+  });
+});
+
+test('should setup new recipe action object', () => {
+  const action = addRecipe({ title: 'New Recipe', yield: '10pax' });
+  expect(action).toEqual({
+    type: 'ADD_RECIPE',
+    payload: { title: 'New Recipe', yield: '10pax' }
+  });
+});
